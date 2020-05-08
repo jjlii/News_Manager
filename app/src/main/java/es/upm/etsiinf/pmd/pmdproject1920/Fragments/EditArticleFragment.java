c830afd49d7a661a0817af8daf991cf15faf5e38
LaertesPecker <marcoslaertes@outlook.com>
2020-05-08 21:00:13 +0200

    first commit

---
+++ app/src/main/java/es/upm/etsiinf/pmd/pmdproject1920/Fragments/EditArticleFragment.java
@@ -0,0 +1,33 @@
+package es.upm.etsiinf.pmd.pmdproject1920.Fragments;
+
+import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import es.upm.etsiinf.pmd.pmdproject1920.MainActivity;
+import es.upm.etsiinf.pmd.pmdproject1920.R;
+import es.upm.etsiinf.pmd.pmdproject1920.utils.network.ModelManager;
+
+import static androidx.core.app.ActivityCompat.startActivityForResult;
+
+public class EditArticleFragment extends Fragment {}
